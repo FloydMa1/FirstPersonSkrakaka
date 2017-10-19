c90ae3996b9283a842389ff73a566e6450d5088a
FloydMa1 <22355@ma-web.nl>
2017-10-19 11:40:05 +0200

    First Commit

---
+++ Assets/Scripts/Player/Shooting/PlayerShoot.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+
+    //public int damagePerShot = 20;
+    public float timeBetweenBullets = 0.15f;
+    public float range = 100f;
+
+
+    float timer;
+    Ray shootRay = new Ray();
+    RaycastHit shootHit;
+    //int shootableMask;
+    ParticleSystem gunParticles;
+    LineRenderer gunLine;
+    float effectsDisplayTime = 0.2f;
+
+
+    void Awake()
+    {
+        gunParticles = GetComponent<ParticleSystem>();
+        gunLine = GetComponent<LineRenderer>();
+    }
+
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (Input.GetMouseButton(0) && timer >= timeBetweenBullets && Time.timeScale != 0)
+        {
+            Shoot();
+        }
+
+        if (timer >= timeBetweenBullets * effectsDisplayTime)
+        {
+            ToggleShootEffects(false);
+        }
+        Debug.DrawRay(transform.position, shootRay.direction, Color.blue);
+    }
+
+    private void ToggleShootEffects(bool on)
+    {
+        gunLine.enabled = on;
+        gunLine.SetPosition(0, transform.position);
+
+    }
+
+    void Shoot()
+    {
+        timer = 0f;
+
+        gunParticles.Stop();
+        gunParticles.Play();
+
+        ToggleShootEffects(true);
+
+        shootRay.origin = transform.position;
+        shootRay.direction = transform.forward;
+
+        if (Physics.Raycast(shootRay, out shootHit, range))
+        {
+            
+            gunLine.SetPosition(1, shootHit.point);
+        }
+        else
+        {
+            gunLine.SetPosition(1, shootRay.origin + shootRay.direction * range);
+        }
+    }
+}
