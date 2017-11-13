aa9b1fe5801e37842fb7db051d976e1cf168418f
FloydMa1 <22355@ma-web.nl>
2017-11-13 11:11:47 +0100

    map

---
+++ Assets/Scripts/mynamajeff.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mynamajeff : MonoBehaviour {
+
+
+	void Start () {
+        StartCoroutine(Doquery());
+	}
+
+    IEnumerator Doquery()
+    {
+        WWW request = new WWW("http://22355.hosts.ma-cloud.nl/bewijzenmap/p2.1/GPR/moan.php?t_x=20&t_y=30&t_z=101&p_id=3");
+        yield return request;
+    }
+	
+}
